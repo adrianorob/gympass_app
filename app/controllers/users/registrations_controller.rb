04635f1ed1384dd02a61bd87723443aae538c361
Adriano Barroso <wiermann@broou.com>
2016-10-12 15:41:42 -0300

    wip - 2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
-    edit_user_registration_path
-  end
+  # def after_sign_up_path_for(resource)
+  #   edit_user_registration_path
+  # end
 end
