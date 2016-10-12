28df7661bfc4e55a81f4d90052040754ecf022c7
Adriano Barroso <wiermann@broou.com>
2016-10-12 15:01:21 -0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012170848) do
+ActiveRecord::Schema.define(version: 20161012171350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20161012170848) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "type_address"
     t.index ["user_id"], name: "index_useraddresses_on_user_id", using: :btree
   end
 
